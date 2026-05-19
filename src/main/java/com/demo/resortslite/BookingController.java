2f5272bc08e49d050e38137602aaf4520b98674c
Modernize Studio <studio@modernize.com>
2026-05-19 11:44:26 +0000

    Commit for planId: 54

--- src/main/java/com/demo/resortslite/BookingController.java
+++ src/main/java/com/demo/resortslite/BookingController.java
@@ -2,8 +2,8 @@ package com.demo.resortslite;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpSession;
 
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
